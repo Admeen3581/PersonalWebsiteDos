4fa50cbf626e725fcd097c049b08d1638352c1e3
Adam Long <adam.jacob.long@gmail.com>
2024-11-19 04:45:42 -0600

    Init Commit 1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +11,23 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        blueblack: "#021526",
+        deepblue: "#03346E",
+        skyblue: "#6EACDA",
+        lightgray: "#E2E2B6",
+      },
+      fontFamily: {
+        astrospace: ["Astrospace", "sans-serif"],
+        ethnocentric: ["Ethnocentric", "sans-serif"],
+        bluehighway: ["Blue Highway", "sans-serif"],
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    "@tailwindcss/forms",
+    "@tailwindcss/typography",
+    "@tailwindcss/aspect-ratio",
+  ],
+};
+
+export default config;
