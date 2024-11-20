60d3c32d81ddcf84c5239d0fc23a095ee41e5c3b
Adam Long <adam.jacob.long@gmail.com>
2024-11-19 22:25:45 -0600

    imported gradual spacing component
    Signed-off-by: Adam Long <adam.jacob.long@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,28 +11,34 @@ import Particles from "@/components/ui/particles";
 
 function Home() {
   return (
-      <div className="relative min-h-screen">
+      <div>
+          <div>
 
-          <Image
-              src="/conny-schneider-abstract.jpg"
-              alt="background image"
-              className=""
-              fill
-          />
-
-          <div className="absolute inset-0 bg-black opacity-65 z-10"></div>
-
-          <div className='relative z-20'>
-              <Particles
-                  quantity={500}
-                  color={'#FFFFFF'}
-                  size={0.8}
-                  vx={0.1}
-                  vy={0.1}
-                  className='opacity-70'
-              />
           </div>
+          {/*Background*/}
+          <div className="relative min-h-screen">
 
+              <Image
+                  src="/conny-schneider-abstract.jpg"
+                  alt="background image"
+                  className=""
+                  fill
+              />
+
+              <div className="absolute inset-0 bg-black opacity-65 z-10"></div>
+
+              <div className='relative z-20 min-h-screen'>
+                  <Particles
+                      quantity={700}
+                      color={'#FFFFFF'}
+                      size={0.8}
+                      vx={0.1}
+                      vy={-0.1}
+                      className='opacity-70'
+                  />
+              </div>
+
+          </div>
       </div>
 
   );
