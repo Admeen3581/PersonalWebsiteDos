956b23097d2ec0d0df8abf9ddd4753d5fc62372d
Adam Long <adam.jacob.long@gmail.com>
2024-11-19 20:35:36 -0600

    Imported nyxb library for component usage
    Signed-off-by: Adam Long <adam.jacob.long@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,39 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        blueblack: "#021526",
-        deepblue: "#03346E",
-        skyblue: "#6EACDA",
-        lightgray: "#E2E2B6",
-      },
-      fontFamily: {
-        astrospace: ["Astrospace", "sans-serif"],
-        ethnocentric: ["Ethnocentric", "sans-serif"],
-        bluehighway: ["Blue Highway", "sans-serif"],
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			blueblack: '#021526',
+  			deepblue: '#03346E',
+  			skyblue: '#6EACDA',
+  			lightgray: '#E2E2B6'
+  		},
+  		fontFamily: {
+  			ethnocentric: ["Ethnocentric", "sans-serif"],
+  			bluehighway: ["Blue Highway", "sans-serif"]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
     "@tailwindcss/forms",
     "@tailwindcss/typography",
     "@tailwindcss/aspect-ratio",
-  ],
+	  "tailwindcss-animate"
+],
 };
 
 export default config;
