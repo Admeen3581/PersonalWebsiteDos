e3451c0707fa2cdcbb4b34b867d6699078590e80
Adam Long <adam.jacob.long@gmail.com>
2024-11-30 21:52:39 -0600

    implement backgroundNonHome.tsx vibes
    Signed-off-by: Adam Long <adam.jacob.long@gmail.com>

--- src/components/backgroundNonHome.tsx
+++ src/components/backgroundNonHome.tsx
@@ -8,7 +8,11 @@ import Particles from "@/components/ui/particles";
 export const BackgroundNonHome = () => {
     return (
         <div className="absolute inset-0 bg-black">
-            <div className="absolute inset-0 bg-black opacity-70 z-10"></div>
+
+            <div className="absolute -right-60 -top-44 h-60 w-[44rem] transform-gpu md:right-0 bg-gradient-to-r opacity-30 from-[#e2beff] from-[28%] via-[#87a4ee] via-[70%] to-[#b060ff] rotate-[-10deg] rounded-full blur-3xl"></div>
+
+            <div className="absolute -left-60 -top-36 h-60 w-[44rem] transform-gpu md:right-0 bg-gradient-to-r opacity-30 from-[#e2beff] from-[28%] via-[#87a4ee] via-[70%] to-[#b060ff] rotate-[10deg] rounded-full blur-3xl"></div>
+
             <Particles
                 quantity={800}
                 color="#FFFFFF"
@@ -16,8 +20,9 @@ export const BackgroundNonHome = () => {
                 size={0.8}
                 vx={0.1}
                 vy={-0.1}
-                className="absolute inset-0 opacity-70 z-20"
+                className="absolute inset-0 opacity-90 z-20"
             />
+
         </div>
     );
 }
