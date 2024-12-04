e2230e4d8dade8decd0d595e4ddb4f53a2e3e45f
Adam Long <adam.jacob.long@gmail.com>
2024-12-04 10:47:46 -0600

    implement base layout
    Signed-off-by: Adam Long <adam.jacob.long@gmail.com>

--- src/app/philosophies/page.tsx
+++ src/app/philosophies/page.tsx
@@ -4,6 +4,7 @@
  */
 import {BackgroundNonHome} from "@/components/backgroundNonHome";
 import {NavbarNonHome} from "@/components/navbarNonHome";
+import GradualSpacing from "@/components/ui/gradualSpacing";
 
 function Philosophies() {
     return (
@@ -14,9 +15,30 @@ function Philosophies() {
             <div className="relative z-30 flex flex-col items-center justify-center h-full">
                 {/*Navbar component*/}
                 <NavbarNonHome/>
+                {/*Heading*/}
+                <div className='grid grid-cols-3 pt-20'>
+                    <div className='flex flex-col items-start'>
+                        <GradualSpacing
+                            text="PHILOSOPHIES"
+                            className="font-display text-sm text-white font-avalors shadow-inner"
+                            duration={2.0}
+                        />
+                        <br/>
+                        <div className='bg-gray-900 h-1 w-full'></div>
+                        <br/>
+                        <GradualSpacing
+                            text="How I live my life"
+                            className="font-display text-3xl text-white font-avalors shadow-inner"
+                            duration={2.0}
+                        />
+                    </div>
+                </div>
                 {/*Page Content*/}
-                <div className='md:pt-64'>
-
+                <div className="w-10/12 mt-24">
+                    <h1 className='text-2xl text-white shadow-inner text-center font-aptos'>
+                        My life philosophies are guided by lessons I have learned. They are written on my mirror, so
+                        that I can be a reflection of who I want to be.
+                    </h1>
                 </div>
             </div>
         </div>
