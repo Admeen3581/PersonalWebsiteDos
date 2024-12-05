5049c291a803652f353577869fa397e9bac6bd4b
Adam Long <adam.jacob.long@gmail.com>
2024-12-05 16:48:01 -0600

    implement template built
    Signed-off-by: Adam Long <adam.jacob.long@gmail.com>

--- src/app/contact/professional/page.tsx
+++ src/app/contact/professional/page.tsx
@@ -4,6 +4,7 @@
  */
 import {BackgroundNonHome} from "@/components/backgroundNonHome";
 import {NavbarNonHome} from "@/components/navbarNonHome";
+import GradualSpacing from "@/components/ui/gradualSpacing";
 
 function ContactProfessional() {
     return (
@@ -14,6 +15,24 @@ function ContactProfessional() {
             <div className="relative z-30 flex flex-col items-center justify-center h-full">
                 {/*Navbar component*/}
                 <NavbarNonHome/>
+                {/*Heading*/}
+                <div className='grid grid-cols-4 pt-20'>
+                    <div className='flex flex-col items-start'>
+                        <GradualSpacing
+                            text="DOWNLOADS"
+                            className="font-display text-sm text-white font-avalors shadow-inner"
+                            duration={2.0}
+                        />
+                        <br/>
+                        <div className='bg-gray-900 h-1 w-full'/>
+                        <br/>
+                        <GradualSpacing
+                            text="All my links"
+                            className="font-display text-3xl text-white font-avalors shadow-inner"
+                            duration={2.0}
+                        />
+                    </div>
+                </div>
                 {/*Page Content*/}
                 <div className='md:pt-64'>
 
