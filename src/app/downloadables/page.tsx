e90f504f16e634da42dfb76127f8303c4e2cc085
Adam Long <adam.jacob.long@gmail.com>
2024-12-04 23:22:01 -0600

    implement MVP
    Signed-off-by: Adam Long <adam.jacob.long@gmail.com>

--- src/app/downloadables/page.tsx
+++ src/app/downloadables/page.tsx
@@ -4,6 +4,8 @@
  */
 import {BackgroundNonHome} from "@/components/backgroundNonHome";
 import {NavbarNonHome} from "@/components/navbarNonHome";
+import GradualSpacing from "@/components/ui/gradualSpacing";
+import {downloadElements} from "@/app/downloadables/downloadElements";
 
 function Downloadables() {
     return (
@@ -14,9 +16,31 @@ function Downloadables() {
             <div className="relative z-30 flex flex-col items-center justify-center h-full">
                 {/*Navbar component*/}
                 <NavbarNonHome/>
+                {/*Heading*/}
+                <div className='grid grid-cols-4 pt-20'>
+                    <div className='flex flex-col items-start'>
+                        <GradualSpacing
+                            text="DOWNLOADS"
+                            className="font-display text-sm text-white font-avalors shadow-inner"
+                            duration={2.0}
+                        />
+                        <br/>
+                        <div className='bg-gray-900 h-1 w-full'/>
+                        <br/>
+                        <GradualSpacing
+                            text="All my links"
+                            className="font-display text-3xl text-white font-avalors shadow-inner"
+                            duration={2.0}
+                        />
+                    </div>
+                </div>
                 {/*Page Content*/}
-                <div className='md:pt-64'>
-
+                <div className='md:pt-20'>
+                    {downloadElements.map((element) => (
+                        <div key={element.title} className='text-gray-300 text-center text-2xl font-semibold font-aptos py-4 hover:text-blue-600'>
+                            <a href={element.link}>{element.title}</a>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
