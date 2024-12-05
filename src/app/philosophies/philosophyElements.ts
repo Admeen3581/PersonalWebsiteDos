18da121f97523deadef65c98ae1d6234794ecf8c
Adam Long <adam.jacob.long@gmail.com>
2024-12-05 12:31:58 -0600

    implement MVP
    Signed-off-by: Adam Long <adam.jacob.long@gmail.com>

--- src/app/philosophies/philosophyElements.ts
+++ src/app/philosophies/philosophyElements.ts
@@ -5,13 +5,137 @@
 
 //Imports
 import {philosophyProps} from "@/components/philosophyCard";
-import {ArrowUpIcon} from "lucide-react";
+import
+{
+    ArrowsUpFromLine,
+    Banknote, BicepsFlexed, CalendarCheck,
+    CassetteTape,
+    Check, Crown, Drama, Dumbbell, GitBranch,
+    HandHeart, Image, Meh,
+    MessageCircleHeart,
+    Smile, Speech, Split, SquareEqual, Target
+
+} from "lucide-react";
 
 
 export const philosophyElements : philosophyProps[] = [
     {
-        icon: ArrowUpIcon,
-        title: "Big down",
-        desc: "Test description",
+        icon: Smile,
+        title: "Don't be perfect, be memorable",
+        desc: "You are well respected and popular if you are a 10/10. You are remembered if you make your 7/10 life entertaining and fun.",
+    },
+    {
+        icon: Target,
+        title : "Strive to be your best self",
+        desc : "Without effort you cannot expect to excel beyond your own expectations. Excellence lies in the pursuit, " +
+            "not the destination. Growth happens when you aim high and embrace the process."
+    },
+    {
+        icon: Check,
+        title : "Take every opportunity you get",
+        desc : "Opportunities often lead to unexpected growth. You take every opportunity you can get and figure the rest out later." +
+            "Without taking initiative on every experience you can, you're limiting your future growth.",
+    },
+    {
+        icon: Meh,
+        title : "Stay neutral",
+        desc : "Objectivity fosters clarity and fairness, allowing for balanced perspectives in complex situations. A non-biased" +
+            "mind allows for ease of placing yourself into other people's shoes.",
+    },
+    {
+        icon: CassetteTape,
+        title : "Respect your elders",
+        desc : "Wisdom comes with experience, and honoring those who’ve walked before us cultivates humility and gratitude." +
+            "They have built society for you to live in, give them your time, energy, and upmost respect.",
+    },
+    {
+        icon: MessageCircleHeart,
+        title : "Keep your word",
+        desc : "Integrity is the cornerstone of trust. Reliability builds lasting relationships and respect. With enough time," +
+            "your word will become authoritative and people will have zero doubt about you, your words, or your actions.",
+    },
+    {
+        icon: Dumbbell,
+        title : "Masculinity isn't always best",
+        desc : "Being the guy that lifts all the weights and always has his guard up is a guy nobody wants to be around all " +
+            "the time. Allow yourself to become a little personable to the world around you without having to be the 'super" +
+            "duper alpha male'",
+    },
+    {
+        icon: Banknote,
+        title : "Being poor is a mentality, don't be poor",
+        desc : "You can be broke, not well, or in the trenches; however, by saying you are poor your mind is accepting the" +
+            "fate of being stuck in a state of being poor. You can work to fix being broke. You can recover to be well. You can" +
+            "climb out of the trenches. You cannot escape being poor.",
+    },
+    {
+        icon: Image,
+        title : "Be the first to paint your own picture",
+        desc : "Allow you to show your abilities, personality, and talent before anyone else has the opportunity to." +
+            "You want to be the one explaining yourself, not the person next to you.",
+    },
+    {
+        icon: Crown,
+        title : "Live life like a game of chess",
+        desc : "You can only make moves and cannot take moves back. All you can do is look at your current position and" +
+            "choose the best move to progress. You cannot sit around wondering why you did not do this, or did not do that." +
+            "Your move can be forwards, backwards, or if you think outside the box, an L shape." +
+            "By not doing something because you might regret it later is how you are holding yourself back.",
+    },
+    {
+        icon: HandHeart,
+        title : "Good leaders have good hearts",
+        desc : "The best leaders make sure they're team members are taken care of before they are. Putting themselves at" +
+            "the bottom and ensuring everyone is heard.",
+    },
+    {
+        icon: BicepsFlexed,
+        title : "Don't be a nice guy, be a good man",
+        desc : "A \"nice guy\" might focus on being agreeable or seeking approval, often prioritizing how they're perceived " +
+            "over their actions or principles. In contrast, a \"good man\" lives by strong moral values, takes responsibility," +
+            " and does what’s right—even when it’s difficult or unpopular.",
+    },
+    {
+        icon: GitBranch,
+        title : "Be proactive, not reactive",
+        desc : "Taking initiative leads to control and preparedness, preventing stress from unexpected challenges. Everything" +
+            "becomes within your control versus reacting to the control of the other force.",
+    },
+    {
+        icon: Split,
+        title : "There are always more than two options",
+        desc : "Thinking beyond binary choices reveals creative solutions and broader possibilities. If someone is pointing" +
+            "a gun at your head: pull out a bigger gun, call their bluff, take their gun.",
+    },
+    {
+        icon: ArrowsUpFromLine,
+        title : "Expect from others what you are willing to put in yourself",
+        desc : "Mutual accountability fosters fairness and respect in all relationships. Hypocrisy is the demise of all" +
+            "people.",
+    },
+    {
+        icon: CalendarCheck,
+        title : "Don't schedule fun, make your work fun",
+        desc : "Everyone only has so much time in their day. Don't spend it painfully washing the dishes or sitting in an" +
+            "office chair. Make work fun! Turn washing the dishes into a speed game. Setup a Bingo card in the office. Find" +
+            "a way to make everything entertaining.",
+    },
+    {
+        icon: Speech,
+        title : "Peer pressure is a hoax",
+        desc : "True independence comes from following your values, not conforming to others’ expectations. By setting your" +
+            "boundaries in stone, others will understand they cannot move you.",
+    },
+    {
+        icon: SquareEqual,
+        title : "Treat everyone as equal",
+        desc : "Regardless of religion, race, or socioeconomic beliefs. Equality fosters harmony and respect, breaking " +
+            "down barriers for a more inclusive world. Everyone deserves the right to have equal opportunity, with outcome" +
+            "in direct relation to the effort put forward.",
+    },
+    {
+        icon: Drama,
+        title : "Respect is given until no longer deserved, then you have to earn it back",
+        desc : "Respect is a shared baseline, but actions determine whether it’s maintained or lost.",
     },
 ];
